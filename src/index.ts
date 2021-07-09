e4c5c74d63a09d5a2e474822511a8ad99eaf9d8c
Fateh Farooqui <farooquifateh184@gmail.com>
2021-07-09 06:20:35 +0530

    Changes

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import AudioPlayer from "./Components/AudioPlayer";
-import usePlayerProgress from "./Hooks/usePlayerProgress";
-import PlayerManager from "./Utils/PlayerManager";
-
-export { AudioPlayer, PlayerManager, usePlayerProgress };
